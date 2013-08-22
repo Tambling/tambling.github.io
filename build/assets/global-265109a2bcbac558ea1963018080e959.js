9770157fbcdb894e4260d2987ddd86b3f6f75aa3
Dean Tambling <dean.tambling@gmail.com>
2013-08-22 01:32:16 -0700

    Moved back to Jekyll-Sass

--- build/assets/global-265109a2bcbac558ea1963018080e959.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $("a[href='#about']").click(function(event){
-    event.preventDefault();
-    $('#about').toggle();
-    $("#floater").toggleClass("opaque bordered");
-    $(this).toggleClass('active');
-  });
-});
